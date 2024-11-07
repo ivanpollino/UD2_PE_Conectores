b8d498c7b967cec53bd200530a3728b2698b5f47
IvanPollino <92946309+ivanpollino@users.noreply.github.com>
2024-11-07 20:15:26 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/ud2_pe_conectores/UD2_PE_Conectores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ud2_pe_conectores;
+
+/**
+ *
+ * @author polli
+ */
+public class UD2_PE_Conectores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
